3fd80793a10c95f1725e838840cee26053041556
MAXIMILLIAN <MAXIMILLIAN@KEPIN>
2023-04-16 20:03:09 +0700

    Add project files.

---
+++ tugas7/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace tugas7
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.gambar_film = new System.Windows.Forms.ImageList(this.components);
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // gambar_film
+            // 
+            this.gambar_film.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("gambar_film.ImageStream")));
+            this.gambar_film.TransparentColor = System.Drawing.Color.Transparent;
+            this.gambar_film.Images.SetKeyName(0, "images (7).jpg");
+            this.gambar_film.Images.SetKeyName(1, "1650826663.jpg");
+            this.gambar_film.Images.SetKeyName(2, "poster-film-yang-tayang-di-bioskop-april-2023-9.jpeg");
+            this.gambar_film.Images.SetKeyName(3, "Screen_Shot_2021_10_20_at_12_13_24.png");
+            this.gambar_film.Images.SetKeyName(4, "sedeng_92whatsapp-image-2022-11-20-at-09.57.16.jpeg");
+            this.gambar_film.Images.SetKeyName(5, "0b7f50354ec4ef04f5cd50abc512232a.jpg");
+            this.gambar_film.Images.SetKeyName(6, "last-night-in-soho.jpeg");
+            this.gambar_film.Images.SetKeyName(7, "310890273_129523326532590_6791100282573647555_n.webp.jpg");
+            // 
+            // panel
+            // 
+            this.panel.AutoScroll = true;
+            this.panel.Location = new System.Drawing.Point(12, 564);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1268, 368);
+            this.panel.TabIndex = 0;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1292, 637);
+            this.Controls.Add(this.panel);
+            this.Name = "main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList gambar_film;
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ tugas7/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tugas7
+{
+    public partial class main : Form      
+    {
+        int nama = 1;        
+        int simpanjam=0;
+        int jarak_batas = 0;
+        int jarak_batas1 = 0;
+        int jaraks = 0;
+        int jarakb = 0;
+        int kontol = 0;
+        /////////////////////////////
+        Button cancle = new Button();
+        Button ok = new Button();
+        Label kuntul = new Label();
+        Random rwarna = new Random();
+        /////////////////////////////
+        List <string> jadwal = new List<string>() { "08:30", "12:30", "14:30", "08:30", "12:30", "14:30", "08:30", "12:30", "14:30", "08:30", "12:30", "14:30", "08:30", "12:30", "14:30", "08:30", "12:30", "14:30", "08:30", "12:30", "14:30", "08:30", "12:30", "14:30", };
+        List<Button> buttons = new List<Button>();
+        List<Button> butkursi = new List<Button>();
+        List<List<Button>>  simpankursi= new List<List<Button>>();
+        public main()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+            kuntul.AutoSize = true;
+            kuntul.Text = "Kursi yang di pilih: ";
+            kuntul.Font = new Font("Arial", 12, FontStyle.Bold);
+            kuntul.Location = new Point(1000, 850);
+            panel.AutoSize = true;
+            ////////////////////////////
+            ok.Size = new Size(140,60);
+            ok.Location = new Point(500, 850);
+            ok.BackColor = Color.DarkCyan;
+            ok.ForeColor = Color.White;
+            ok.Text = "GASKEUN";            
+            ok.Click += setuju;   
+            ///////////////////////////
+            cancle.Size = new Size(140, 60);
+            cancle.Location = new Point(800, 850);
+            cancle.BackColor = Color.LightPink;
+            cancle.ForeColor = Color.White;
+            cancle.Text = "GAJADI";            
+            cancle.Click += gsetuju;
+            ///////////////////////////
+            for (int i=0;i<8;i++)
+            {
+                PictureBox film = new PictureBox();
+                film.Size = new Size(180, 300);
+                film.Location = new Point(20+jarak_batas, 20);
+                film.Image = gambar_film.Images[i];
+                this.Controls.Add(film);               
+                jarak_batas+= 200;
+            }
+            ///////////////////////////
+            jarak_batas=0;
+            for(int i=0;i<24;i++)
+            {
+                if (i % 3 == 0)
+                {
+                    jarak_batas1 += 200;
+                    jarak_batas = 0;
+                }
+                Button pesen = new Button();
+                pesen.Location = new Point(-130 + jarak_batas1, 330 + jarak_batas);
+                pesen.AutoSize = true;
+                pesen.Text = jadwal[i];
+                pesen.BackColor = Color.LightGreen;
+                pesen.Tag = i.ToString();
+                this.Controls.Add(pesen);
+                jarak_batas += 30;
+                buttons.Add(pesen);
+                pesen.Click += masok;        
+            }      
+            ////////////////////////////
+            for (int z = 0; z < 24; z++)
+            {
+                nama = 1;
+                jarakb = 0;
+                butkursi = new List<Button>();
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 20; j++)
+                    {
+                        Button kursi = new Button();                       
+                        kursi.Size = new Size(60, 60);
+                        kursi.Location = new Point(150 + jaraks, 5 + jarakb);
+                        kursi.BackColor = Color.LightGreen;
+                        kursi.Tag = "[kursi " + nama.ToString() + "]";
+                        butkursi.Add(kursi);
+                        jaraks += 70;
+                        kursi.Click += nonton;
+                        nama++;
+                    }
+                    jaraks = 0;
+                    jarakb += 70;                   
+                }
+                for (int i = 0; i < 70; i++)
+                {
+                    int simpan = rwarna.Next(0, butkursi.Count);
+                    butkursi[simpan].BackColor = Color.HotPink;
+                    butkursi[simpan].Enabled = false;
+                }
+                simpankursi.Add(butkursi);
+            }            
+        }
+        private void masok(object sender, EventArgs e)
+        {
+            foreach (Button x in simpankursi[simpanjam])
+            {
+                if (x.BackColor == Color.Yellow)
+                {
+                    x.BackColor = Color.LightGreen;
+                }
+            }
+            kuntul.Text = "Kursi yang di pilih: ";
+
+            Button but = sender as Button;
+            simpanjam = Convert.ToInt32(but.Tag);
+            
+            foreach (Button b in buttons)
+            {
+                if (b.Tag == but.Tag)
+                {
+                    panel.Controls.Clear();
+                    for (int i = 0; i<100; i++)
+                    {                       
+                        panel.Controls.Add(simpankursi[Convert.ToInt32(but.Tag)][i]);                       
+                    }     
+                }
+            }
+            
+            this.Controls.Add(ok);
+            this.Controls.Add(cancle);
+            this.Controls.Add(kuntul);
+        }
+        private void nonton (object sender, EventArgs e)
+        {
+            Button memek = sender as Button;
+            if(memek.BackColor == Color.LightGreen)
+            {
+                memek.BackColor = Color.Yellow;
+            }
+            else if(memek.BackColor == Color.Yellow)
+            {
+                memek.BackColor = Color.LightGreen;
+            }
+            kuntul.Text += memek.Tag + " ";
+        }
+        private void setuju (object sender, EventArgs e)
+        {
+            foreach (Button x in simpankursi[simpanjam])
+            { 
+                if(x.BackColor == Color.Yellow)
+                {
+                    x.BackColor = Color.HotPink;
+                    kuntul.Text = "Kursi yang di pilih: ";
+                }
+            }
+        }
+        private void gsetuju(object sender, EventArgs e)
+        {
+
+            foreach (Button x in simpankursi[simpanjam])
+            {
+                x.Enabled = true;
+                x.BackColor = Color.LightGreen;
+                kuntul.Text = "Kursi yang di pilih: ";
+            }
+        }
+    }
+}
